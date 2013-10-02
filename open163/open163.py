38738c5f502b8275a24a657e0e06056a8539c270
wwq0327 <wwq0327@gmail.com>
2013-10-02 18:33:34 +0800

    f

--- open163/open163.py
+++ open163/open163.py
@@ -26,15 +26,28 @@ class OpenClass:
         '''获取当页所有TED视频的播放页面链接'''
 
         html = html.replace('\n', '').strip()
-        regex = r'<li class="f-fl">\s+<a href="(.+?)".+?<p>(.+?)</p>.+?</a>\s+</li>'
+        regex = r'<li class="f-fl">\s+<a href="(http://v.163.com/movie/.+?)" class="listT">.+?<p>(.+?)<(/p|p)>.+?</a>\s+</li>'
         p_re = re.compile(regex, re.DOTALL)
         pages_list = p_re.findall(html)
-        print len(pages_list)
+        #print len(pages_list)
         return pages_list
+
+    def get_video_url(self, url):
+        r = requests.get(url)
+        if r.status_code == 200:
+            html = r.content
+        else:
+            html = None
+        p = re.compile(r'appsrc: \'http://mov\.bn\.netease\.com/.+?\.m3u8\',', re.DOTALL)
+        end = p.findall(html)
+
+        return end[0]
 
     def play_page(self, page_list):
         for page in page_list:
-            print "%s ==> %s" % (page[0], page[1])
+            p = self.get_video_url(page[0])
+            print p
+            #print "%s ==> %s" % (page[0], page[1].decode('gb2312').encode('utf-8'))
 
 
 def main():
